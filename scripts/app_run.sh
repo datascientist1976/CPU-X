ea43d3e27241e85ff0bda9d7696dbff698e1ced0
Xorg <xorgbreaker@gmail.com>
2020-05-05 21:01:40 +0200

    Build AppImage

--- scripts/app_run.sh
+++ scripts/app_run.sh
@@ -11,12 +11,14 @@ else
 fi
 
 APPDIR="$(dirname "$(realpath "$0")")"
+_GDK_PIXBUF_MODULEDIR="/usr/lib/gdk-pixbuf-2.0/2.10.0/loaders"
 export PATH="$APPDIR/usr/local/sbin:$APPDIR/usr/local/bin:$APPDIR/usr/sbin:$APPDIR/usr/bin:$APPDIR/sbin:$APPDIR/bin"
 export LD_LIBRARY_PATH="$APPDIR/usr/lib/:$APPDIR/usr/lib/i386-linux-gnu/:$APPDIR/usr/lib/x86_64-linux-gnu/:$APPDIR/usr/lib32/:$APPDIR/usr/lib64/:$APPDIR/lib/:$APPDIR/lib/i386-linux-gnu/:$APPDIR/lib/x86_64-linux-gnu/:$APPDIR/lib32/:$APPDIR/lib64/:$APPDIR"
 export GTK_DATA_PREFIX="$APPDIR"
 export GTK_PATH="$APPDIR/usr/lib/gtk-3.0"
 export GTK_IM_MODULE_FILE="$APPDIR/usr/lib/gtk-3.0/3.0.0/immodules.cache"
-export GDK_PIXBUF_MODULE_FILE="$APPDIR/usr/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache"
+export GDK_PIXBUF_MODULEDIR="$APPDIR/$_GDK_PIXBUF_MODULEDIR"
+export GDK_PIXBUF_MODULE_FILE="$APPDIR/$_GDK_PIXBUF_MODULEDIR/loaders.cache"
 export GSETTINGS_SCHEMA_DIR="$APPDIR/usr/share/glib-2.0/schemas"
 export PANGO_LIBDIR="$APPDIR/usr/lib"
 export PANGO_SYSCONFDIR="$APPDIR/etc"
@@ -26,4 +28,5 @@ export TEXTDOMAINDIR="$APPDIR/usr/share/locale"
 export TERMINFO="$APPDIR/usr/share/terminfo"
 
 cd "$APPDIR"
+sed -i -e "s|$_GDK_PIXBUF_MODULEDIR|$GDK_PIXBUF_MODULEDIR|g" "$GDK_PIXBUF_MODULE_FILE"
 $EXEC "$APPDIR/usr/bin/cpu-x" "$@"
--- scripts/build_appimage.sh
+++ scripts/build_appimage.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-BUILD_APPDIR="$1"
-echo "BUILD_APPDIR=$BUILD_APPDIR"
-ls "$BUILD_APPDIR"
+APPDIR="$1"
 
-#cp -v "$BUILD_APPDIR/usr/share/applications/cpu-x.desktop" "$BUILD_APPDIR/"
-#cp -v "$BUILD_APPDIR/usr/share/icons/hicolor/96x96/apps/cpu-x.png" "$BUILD_APPDIR/"
+#cp -v "$APPDIR/usr/share/applications/cpu-x.desktop" "$APPDIR/"
+#cp -v "$APPDIR/usr/share/icons/hicolor/96x96/apps/cpu-x.png" "$APPDIR/"
 
-#mkdir -pv "$BUILD_APPDIR/usr/share/"
-#cp -Rv "/lib/terminfo/" "$BUILD_APPDIR/lib/"
+#mkdir -pv "$APPDIR/usr/share/"
+#cp -Rv "/lib/terminfo/" "$APPDIR/lib/"
 
-#mkdir -pv "$BUILD_APPDIR/usr/lib/x86_64-linux-gnu/"
-#cp -Rv "/usr/lib/x86_64-linux-gnu/gdk-pixbuf-2.0" "$BUILD_APPDIR/usr/lib/x86_64-linux-gnu/"
+#mkdir -pv "$APPDIR/usr/lib/x86_64-linux-gnu/"
+#cp -Rv "/usr/lib/x86_64-linux-gnu/gdk-pixbuf-2.0" "$APPDIR/usr/lib/x86_64-linux-gnu/"
 
 wget -c -nv -O "linuxdeployqt.AppImage" "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 chmod a+x "linuxdeployqt.AppImage"
@@ -19,10 +17,17 @@ unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
 
 #export VERSION=$(git describe --long --tags | sed 's/^v//;s/\([^-]*-g\)/r\1/;s/-/./g') # linuxdeployqt uses this for naming the file
 export VERSION="continuous"
-cp -v "./scripts/app_run.sh" "$BUILD_APPDIR/AppRun"
-./linuxdeployqt.AppImage "$BUILD_APPDIR/usr/share/applications/cpu-x.desktop" -appimage -bundle-non-qt-libs -verbose=2
-#rm -rfv $BUILD_APPDIR/{etc,var}
-find $BUILD_APPDIR/
+gtk_libdir=$(pkg-config --variable=libdir gtk+-3.0)
+gdk_pixbuf_moduledir="$(pkg-config --variable=gdk_pixbuf_moduledir gdk-pixbuf-2.0)"
+gdk_pixbuf_cache_file="$(pkg-config --variable=gdk_pixbuf_cache_file gdk-pixbuf-2.0)"
+gdk_pixbuf_dir="$(dirname "$gdk_pixbuf_moduledir")"
+mkdir -pv "$gdk_pixbuf_dir"
+cp -Rv "$gtk_libdir/gtk-3.0" "$APPDIR/usr/lib"
+cp -Rv "$gdk_pixbuf_moduledir" "$gdk_pixbuf_cache_file" "$APPDIR/$gdk_pixbuf_dir"
+cp -v "./scripts/app_run.sh" "$APPDIR/AppRun"
+./linuxdeployqt.AppImage "$APPDIR/usr/share/applications/cpu-x.desktop" -appimage -bundle-non-qt-libs -verbose=2
+#rm -rfv $APPDIR/{etc,var}
+find $APPDIR/
 find . -name '*.AppImage' -exec realpath {} \;
 
 # wget -c https://github.com/probonopd/uploadtool/raw/master/upload.sh
