9096de37c950beab3a2f9ec90a356aa5c9394293
Xorg <xorgbreaker@gmail.com>
2020-05-05 21:31:08 +0200

    Build AppImage

--- scripts/build_appimage.sh
+++ scripts/build_appimage.sh
@@ -2,28 +2,23 @@
 
 APPDIR="$1"
 
-#cp -v "$APPDIR/usr/share/applications/cpu-x.desktop" "$APPDIR/"
-#cp -v "$APPDIR/usr/share/icons/hicolor/96x96/apps/cpu-x.png" "$APPDIR/"
-
-#mkdir -pv "$APPDIR/usr/share/"
-#cp -Rv "/lib/terminfo/" "$APPDIR/lib/"
-
-#mkdir -pv "$APPDIR/usr/lib/x86_64-linux-gnu/"
-#cp -Rv "/usr/lib/x86_64-linux-gnu/gdk-pixbuf-2.0" "$APPDIR/usr/lib/x86_64-linux-gnu/"
-
 wget -c -nv -O "linuxdeployqt.AppImage" "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 chmod a+x "linuxdeployqt.AppImage"
 unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
 
 #export VERSION=$(git describe --long --tags | sed 's/^v//;s/\([^-]*-g\)/r\1/;s/-/./g') # linuxdeployqt uses this for naming the file
 export VERSION="continuous"
-gtk_libdir=$(pkg-config --variable=libdir gtk+-3.0)
+gtk_libdir="$(pkg-config --variable=libdir gtk+-3.0)"
 gdk_pixbuf_moduledir="$(pkg-config --variable=gdk_pixbuf_moduledir gdk-pixbuf-2.0)"
 gdk_pixbuf_cache_file="$(pkg-config --variable=gdk_pixbuf_cache_file gdk-pixbuf-2.0)"
 gdk_pixbuf_dir="$(dirname "$gdk_pixbuf_moduledir")"
+gsettings_schema_dir="$(pkg-config --variable=schemasdir gio-2.0)"
+terminfo_dir="$(pkg-config --variable=libdir tinfo)"
 mkdir -pv "$gdk_pixbuf_dir"
-cp -Rv "$gtk_libdir/gtk-3.0" "$APPDIR/usr/lib"
-cp -Rv "$gdk_pixbuf_moduledir" "$gdk_pixbuf_cache_file" "$APPDIR/$gdk_pixbuf_dir"
+cp -Rv "$gtk_libdir/gtk-3.0" "$APPDIR/$gtk_libdir/"
+cp -Rv "$gdk_pixbuf_moduledir" "$gdk_pixbuf_cache_file" "$APPDIR/$gdk_pixbuf_dir/"
+cp -Rv "$terminfo_dir/terminfo" "$APPDIR/$terminfo_dir/"
+cp -v "$gsettings_schema_dir/"* "$APPDIR/$gsettings_schema_dir/"
 cp -v "./scripts/app_run.sh" "$APPDIR/AppRun"
 ./linuxdeployqt.AppImage "$APPDIR/usr/share/applications/cpu-x.desktop" -appimage -bundle-non-qt-libs -verbose=2
 #rm -rfv $APPDIR/{etc,var}
