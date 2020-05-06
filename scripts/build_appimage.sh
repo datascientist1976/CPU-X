74908c877c7951274f72537bbb0d5cb651dd272f
Xorg <xorgbreaker@gmail.com>
2020-05-06 13:22:00 +0200

    Build AppImage

--- scripts/build_appimage.sh
+++ scripts/build_appimage.sh
@@ -32,29 +32,30 @@ gsettings_schema_dir="/usr/share/glib-2.0/schemas"
 #cp -Rv "$gdk_pixbuf_moduledir" "$gdk_pixbuf_cache_file" "$APPDIR/$gdk_pixbuf_dir/"
 #cp -Rv "/usr/share/"{glib-2.0,terminfo,themes} "$APPDIR/usr/share/"
 #cp -v "/usr/lib/x86_64-linux-gnu/librsvg"* "$APPDIR/usr/lib/x86_64-linux-gnu/"
-safeCopy "$gtk_libdir/gtk-2.0" \
+safeCopy \
 	"$gtk_libdir/gtk-3.0" \
 	"$gdk_pixbuf_moduledir" \
 	"$gdk_pixbuf_cache_file" \
 	"/usr/share/"{glib-2.0,icons,terminfo,themes} \
-	"/usr/lib/gnome-settings-daemon-3.0" \
-	"/usr/lib/x86_64-linux-gnu/libgdk_pixbuf-2.0"* \
-	"/usr/lib/x86_64-linux-gnu/librsvg"* \
-	"/lib/x86_64-linux-gnu/"* \
 	"$APPDIR"
-runCmd cp -v "./scripts/app_run.sh" "$APPDIR/AppRun"
+#runCmd cp -v "./scripts/app_run.sh" "$APPDIR/AppRun"
 runCmd glib-compile-schemas "$APPDIR/$gsettings_schema_dir"
 
-runCmd wget -c -nv -O "linuxdeployqt.AppImage" "https://github.com/probonopd/linuxdeployqt/releases/download/6/linuxdeployqt-6-x86_64.AppImage"
-#runCmd wget -c -nv -O "linuxdeployqt.AppImage" "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
-runCmd chmod a+x "linuxdeployqt.AppImage"
-unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
+#runCmd wget -c -nv -O "linuxdeploy.AppImage" "https://github.com/probonopd/linuxdeployqt/releases/download/6/linuxdeployqt-6-x86_64.AppImage"
+#runCmd wget -c -nv -O "linuxdeploy.AppImage" "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
+runCmd wget -c -nv -O "linuxdeploy.AppImage" "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage"
+runCmd chmod a+x "linuxdeploy.AppImage"
+#unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
 export ARCH=x86_64
 #export VERSION=$(git describe --long --tags | sed 's/^v//;s/\([^-]*-g\)/r\1/;s/-/./g') # linuxdeployqt uses this for naming the file
 export VERSION="continuous"
-./linuxdeployqt.AppImage \
-	"$APPDIR/usr/share/applications/cpu-x.desktop" \
-	-appimage \
-	-unsupported-bundle-everything \
-	-no-copy-copyright-files \
-	-verbose=2
+# ./linuxdeploy.AppImage \
+# 	"$APPDIR/usr/share/applications/cpu-x.desktop" \
+# 	-appimage \
+# 	-unsupported-bundle-everything \
+# 	-no-copy-copyright-files \
+# 	-verbose=2
+./linuxdeploy.AppImage \
+	--appdir="$APPDIR" \
+	--desktop-file="$APPDIR/usr/share/applications/cpu-x.desktop" \
+	--verbosity=1
