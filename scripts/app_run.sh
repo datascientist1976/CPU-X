b44b49b889d6794fbd2796697f656cd08ed6ca68
Xorg <xorgbreaker@gmail.com>
2020-05-06 21:16:17 +0200

    Build AppImage

--- scripts/app_run.sh
+++ scripts/app_run.sh
@@ -35,22 +35,20 @@ if [[ -n "$CPUX_APPIMAGE_GDB" ]]; then
 else
 	EXEC=exec
 fi
-echo "APPDIR=$APPDIR"
-APPDIR="$(dirname "$(realpath "$0")")"
+
 CACHEDIR="$(mktemp --tmpdir --directory CPU-X.XXXXXXXX)"
-_GTK_IM_DIR="/usr/lib/x86_64-linux-gnu/gtk-3.0/3.0.0"
-_GDK_PIXBUF_DIR="/usr/lib/x86_64-linux-gnu/gdk-pixbuf-2.0/2.10.0"
-#export GTK_THEME=Adwaita
+
+export GTK_THEME=Adwaita
 export GDK_BACKEND=x11
 safeExport GTK_MODULES="$APPDIR/usr/lib/x86_64-linux-gnu/gtk-2.0/modules"
 safeExport GTK3_MODULES="$APPDIR/usr/lib/gnome-settings-daemon-3.0/gtk-modules"
 safeExport GTK_DATA_PREFIX="$APPDIR"
 safeExport GTK_EXE_PREFIX="$APPDIR/usr"
 safeExport GTK_PATH="$APPDIR/usr/lib/x86_64-linux-gnu/gtk-3.0"
-safeExport GTK_IM_MODULE_DIR="$APPDIR/$_GTK_IM_DIR/immodules"
-export GTK_IM_MODULE_FILE="$CACHEDIR/immodules.cache" #"$APPDIR/$_GTK_IM_DIR/immodules.cache"
-safeExport GDK_PIXBUF_MODULEDIR="$APPDIR/$_GDK_PIXBUF_DIR/loaders"
-export GDK_PIXBUF_MODULE_FILE="$CACHEDIR/loaders.cache" #"$APPDIR/$_GDK_PIXBUF_DIR/loaders.cache"
+safeExport GTK_IM_MODULE_DIR="$APPDIR//usr/lib/x86_64-linux-gnu/gtk-3.0/3.0.0/immodules"
+export     GTK_IM_MODULE_FILE="$CACHEDIR/immodules.cache"
+safeExport GDK_PIXBUF_MODULEDIR="$APPDIR/usr/lib/x86_64-linux-gnu/gdk-pixbuf-2.0/2.10.0/loaders"
+export     GDK_PIXBUF_MODULE_FILE="$CACHEDIR/loaders.cache"
 safeExport GSETTINGS_SCHEMA_DIR="$APPDIR/usr/share/glib-2.0/schemas"
 safeExport PANGO_LIBDIR="$APPDIR/usr/lib"
 #safeExport PANGO_SYSCONFDIR="$APPDIR/etc"
@@ -64,24 +62,6 @@ safeExport PATH="$APPDIR/usr/bin:$PATH"
 gtk-query-immodules-3.0 "$GTK_IM_MODULE_DIR/"* > "$GTK_IM_MODULE_FILE"
 gdk-pixbuf-query-loaders "$GDK_PIXBUF_MODULEDIR/"* > "$GDK_PIXBUF_MODULE_FILE"
 
-cd "$APPDIR/"
-if [ -e "$APPDIR/lib/x86_64-linux-gnu/ld-linux-x86-64.so.2" ] ; then
-	echo "Run experimental bundle that bundles everything"
-	export GCONV_PATH="$APPDIR/usr/lib/x86_64-linux-gnu/gconv"
-	export FONTCONFIG_FILE="$APPDIR/etc/fonts/fonts.conf"
-	export LIBRARY_PATH="$APPDIR/usr/lib":$LIBRARY_PATH
-	export LIBRARY_PATH="$APPDIR/lib":$LIBRARY_PATH
-	export LIBRARY_PATH="$APPDIR/usr/lib/i386-linux-gnu":$LIBRARY_PATH
-	export LIBRARY_PATH="$APPDIR/lib/i386-linux-gnu":$LIBRARY_PATH
-	export LIBRARY_PATH="$APPDIR/usr/lib/i386-linux-gnu/pulseaudio":$LIBRARY_PATH
-	export LIBRARY_PATH="$APPDIR/usr/lib/i386-linux-gnu/alsa-lib":$LIBRARY_PATH
-	export LIBRARY_PATH="$APPDIR/usr/lib/x86_64-linux-gnu":$LIBRARY_PATH
-	export LIBRARY_PATH="$APPDIR/lib/x86_64-linux-gnu":$LIBRARY_PATH
-	export LIBRARY_PATH="$APPDIR/usr/lib/x86_64-linux-gnu/pulseaudio":$LIBRARY_PATH
-	export LIBRARY_PATH="$APPDIR/usr/lib/x86_64-linux-gnu/alsa-lib":$LIBRARY_PATH
-	export LIBRARY_PATH=$GDK_PIXBUF_MODULEDIR:$LIBRARY_PATH # Otherwise getting "Unable to load image-loading module"
-	$EXEC "$APPDIR/lib/x86_64-linux-gnu/ld-linux-x86-64.so.2" --inhibit-cache --library-path "$LIBRARY_PATH" "$APPDIR/usr/bin/cpu-x" "$@"
-else
-	$EXEC "$APPDIR/usr/bin/cpu-x" "$@"
-fi
+cd "$APPDIR"
+$EXEC "$APPDIR/usr/bin/cpu-x" "$@"
 rm -rf "$CACHEDIR"
--- scripts/build_appimage.sh
+++ scripts/build_appimage.sh
@@ -25,46 +25,28 @@ APPDIR="$1"
 gtk_libdir="$(pkg-config --variable=libdir gtk+-3.0)"
 gdk_pixbuf_moduledir="$(pkg-config --variable=gdk_pixbuf_moduledir gdk-pixbuf-2.0)"
 gdk_pixbuf_cache_file="$(pkg-config --variable=gdk_pixbuf_cache_file gdk-pixbuf-2.0)"
-gdk_pixbuf_dir="$(dirname "$gdk_pixbuf_moduledir")"
 gsettings_schema_dir="/usr/share/glib-2.0/schemas"
-#mkdir -pv "$APPDIR/$gtk_libdir" "$APPDIR/$gdk_pixbuf_dir" "$APPDIR/$terminfo_dir" "$APPDIR/$gsettings_schema_dir"
-#cp -Rv "$gtk_libdir/gtk-3.0" "$APPDIR/$gtk_libdir/"
-#cp -Rv "$gdk_pixbuf_moduledir" "$gdk_pixbuf_cache_file" "$APPDIR/$gdk_pixbuf_dir/"
-#cp -Rv "/usr/share/"{glib-2.0,terminfo,themes} "$APPDIR/usr/share/"
-#cp -v "/usr/lib/x86_64-linux-gnu/librsvg"* "$APPDIR/usr/lib/x86_64-linux-gnu/"
 safeCopy \
-	"$gtk_libdir/"{gtk-2.0,gtk-3.0} \
+	"$gtk_libdir/gtk-3.0" \
 	"$gdk_pixbuf_moduledir" \
 	"$gdk_pixbuf_cache_file" \
 	"/usr/share/"{glib-2.0,icons,terminfo,themes} \
 	"/usr/lib/x86_64-linux-gnu/libgdk_pixbuf"* \
 	"/usr/lib/x86_64-linux-gnu/libgobject"* \
-	"/usr/lib/x86_64-linux-gnu/libcanberra"* \
-	"/usr/lib/gnome-settings-daemon-3.0" \
 	"$APPDIR"
 runCmd cp -v "./scripts/app_run.sh" "$APPDIR/AppRun"
-#runCmd glib-compile-schemas "$APPDIR/$gsettings_schema_dir"
+runCmd glib-compile-schemas "$APPDIR/$gsettings_schema_dir"
 
-#runCmd wget -c -nv -O "linuxdeploy.AppImage" "https://github.com/probonopd/linuxdeployqt/releases/download/6/linuxdeployqt-6-x86_64.AppImage"
-#runCmd wget -c -nv -O "linuxdeploy.AppImage" "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
-runCmd wget -c "https://raw.githubusercontent.com/linuxdeploy/linuxdeploy-plugin-gtk/master/linuxdeploy-plugin-gtk.sh"
 runCmd wget -c -nv -O "linuxdeploy.AppImage" "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage"
 runCmd chmod -v a+x *.sh *.AppImage
-#unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
 export ARCH=x86_64
 #export VERSION=$(git describe --long --tags | sed 's/^v//;s/\([^-]*-g\)/r\1/;s/-/./g') # linuxdeployqt uses this for naming the file
 export VERSION="continuous"
 # set environment variable to embed update information in an AppImage
 #export UPDATE_INFORMATION="zsync|https://foo.bar/myappimage-latest.AppImage.zsync"
 export VERBOSE=1
-export NO_STRIP=1
 export DISABLE_COPYRIGHT_FILES_DEPLOYMENT=1
-# ./linuxdeploy.AppImage \
-# 	"$APPDIR/usr/share/applications/cpu-x.desktop" \
-# 	-appimage \
-# 	-unsupported-bundle-everything \
-# 	-no-copy-copyright-files \
-# 	-verbose=2
+
 ./linuxdeploy.AppImage \
 	--appdir="$APPDIR" \
 	--desktop-file="$APPDIR/usr/share/applications/cpu-x.desktop" \
