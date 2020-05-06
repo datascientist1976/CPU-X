23bc4c6f4406562e537c4efb03bb1bac1f0fba5d
Xorg <xorgbreaker@gmail.com>
2020-05-06 08:16:17 +0200

    Build AppImage

--- scripts/app_run.sh
+++ scripts/app_run.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+die() {
+	echo "[FATAL] $*"
+	exit 1
+}
+
+safeExport() {
+	local expr="$1"
+	local var="$(echo "$expr" | cut -d= -f1)"
+	local dirs="$(echo "$expr" | cut -d= -f2)"
+	local ok=false
+
+	for dir in $(echo "$dirs" | sed 's/:/ /g'); do
+		if [[ ! -d "$dir" ]]; then
+			local msg="Directory '$dir' does not exist (used by '$var')"
+			if $ok; then
+				echo "$msg"
+			else
+				die "$msg"
+			fi
+			ok=true
+	done
+}
+
 if [[ -n "$CPUX_APPIMAGE_DEBUG" ]]; then
 	set -x
 fi
@@ -11,29 +34,30 @@ else
 fi
 
 APPDIR="$(dirname "$(realpath "$0")")"
+CACHEDIR="$(mktemp --tmpdir --directory CPU-X.XXXXXXXX)"
 _GTK_IM_DIR="/usr/lib/x86_64-linux-gnu/gtk-3.0/3.0.0"
 _GDK_PIXBUF_DIR="/usr/lib/x86_64-linux-gnu/gdk-pixbuf-2.0/2.10.0"
 export GTK_THEME=Adwaita
-export GTK_DATA_PREFIX="$APPDIR"
-export GTK_EXE_PREFIX="$APPDIR/usr"
-export GTK_PATH="$APPDIR/usr/lib/x86_64-linux-gnu/gtk-3.0"
-export GTK_IM_MODULE_DIR="$APPDIR/$_GTK_IM_DIR/immodules"
-export GTK_IM_MODULE_FILE="$APPDIR/$_GTK_IM_DIR/immodules.cache"
-export GDK_PIXBUF_MODULEDIR="$APPDIR/$_GDK_PIXBUF_DIR/loaders"
-export GDK_PIXBUF_MODULE_FILE="$APPDIR/$_GDK_PIXBUF_DIR/loaders.cache"
-export GSETTINGS_SCHEMA_DIR="$APPDIR/usr/share/glib-2.0/schemas"
-export PANGO_LIBDIR="$APPDIR/usr/lib"
-export PANGO_SYSCONFDIR="$APPDIR/etc"
-export XDG_CONFIG_DIRS="$APPDIR/etc/xdg"
-export XDG_DATA_DIRS="$APPDIR/usr/share:/usr/share:$XDG_DATA_DIRS"
-export TEXTDOMAINDIR="$APPDIR/usr/share/locale"
-export TERMINFO="$APPDIR/usr/share/terminfo"
-export LD_LIBRARY_PATH="$APPDIRusr/lib/:$APPDIR/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH:$GDK_PIXBUF_MODULEDIR"
-export PATH="$APPDIR/usr/local/sbin:$APPDIR/usr/local/bin:$APPDIR/usr/sbin:$APPDIR/usr/bin:$APPDIR/sbin:$APPDIR/bin:$PATH"
+safeExport GTK_DATA_PREFIX="$APPDIR"
+safeExport GTK_EXE_PREFIX="$APPDIR/usr"
+safeExport GTK_PATH="$APPDIR/usr/lib/x86_64-linux-gnu/gtk-3.0"
+safeExport GTK_IM_MODULE_DIR="$APPDIR/$_GTK_IM_DIR/immodules"
+export GTK_IM_MODULE_FILE="$CACHEDIR/immodules.cache" #"$APPDIR/$_GTK_IM_DIR/immodules.cache"
+safeExport GDK_PIXBUF_MODULEDIR="$APPDIR/$_GDK_PIXBUF_DIR/loaders"
+export GDK_PIXBUF_MODULE_FILE="$CACHEDIR/loaders.cache" #"$APPDIR/$_GDK_PIXBUF_DIR/loaders.cache"
+safeExport GSETTINGS_SCHEMA_DIR="$APPDIR/usr/share/glib-2.0/schemas"
+safeExport PANGO_LIBDIR="$APPDIR/usr/lib"
+safeExport PANGO_SYSCONFDIR="$APPDIR/etc"
+safeExport XDG_CONFIG_DIRS="$APPDIR/etc/xdg"
+safeExport XDG_DATA_DIRS="$APPDIR/usr/share:/usr/share:$XDG_DATA_DIRS"
+safeExport TEXTDOMAINDIR="$APPDIR/usr/share/locale"
+safeExport TERMINFO="$APPDIR/usr/share/terminfo"
+safeExport LD_LIBRARY_PATH="$APPDIR/usr/lib/:$APPDIR/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH:$GDK_PIXBUF_MODULEDIR"
+safeExport PATH="$APPDIR/usr/bin:$PATH"
 
 gtk-query-immodules-3.0 "$GTK_IM_MODULE_DIR/"* > "$GTK_IM_MODULE_FILE"
 gdk-pixbuf-query-loaders "$GDK_PIXBUF_MODULEDIR/"* > "$GDK_PIXBUF_MODULE_FILE"
-glib-compile-schemas "$GSETTINGS_SCHEMA_DIR"
 
 cd "$APPDIR/"
 $EXEC "$APPDIR/usr/bin/cpu-x" "$@"
+rm -rf "$CACHEDIR"
--- scripts/build_appimage.sh
+++ scripts/build_appimage.sh
@@ -1,29 +1,49 @@
 #!/bin/bash
 
+die() {
+	echo "[FATAL] $*"
+	exit 1
+}
+
+runCmd() {
+	if ! "$@"; then
+		die "command failed: '$*'"
+	fi
+}
+
+safeCopy() {
+	local src="${@:1:$#-1}"
+	local dst="${@:$#}"
+
+	for s in $src; do
+		runCmd cp "$s" --archive --parents --target-directory="$dst" --verbose
+	done
+}
+
 APPDIR="$1"
 
-wget -c -nv -O "linuxdeployqt.AppImage" "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
-chmod a+x "linuxdeployqt.AppImage"
-unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
-
-#export VERSION=$(git describe --long --tags | sed 's/^v//;s/\([^-]*-g\)/r\1/;s/-/./g') # linuxdeployqt uses this for naming the file
-export VERSION="continuous"
 gtk_libdir="$(pkg-config --variable=libdir gtk+-3.0)"
 gdk_pixbuf_moduledir="$(pkg-config --variable=gdk_pixbuf_moduledir gdk-pixbuf-2.0)"
 gdk_pixbuf_cache_file="$(pkg-config --variable=gdk_pixbuf_cache_file gdk-pixbuf-2.0)"
 gdk_pixbuf_dir="$(dirname "$gdk_pixbuf_moduledir")"
-mkdir -pv "$APPDIR/$gtk_libdir" "$APPDIR/$gdk_pixbuf_dir" "$APPDIR/$terminfo_dir" "$APPDIR/$gsettings_schema_dir"
-cp -Rv "$gtk_libdir/gtk-3.0" "$APPDIR/$gtk_libdir/"
-cp -Rv "$gdk_pixbuf_moduledir" "$gdk_pixbuf_cache_file" "$APPDIR/$gdk_pixbuf_dir/"
-cp -Rv "/usr/share/"{glib-2.0,terminfo} "$APPDIR/usr/share/"
-cp -v "/usr/lib/x86_64-linux-gnu/librsvg"* "$APPDIR/usr/lib/x86_64-linux-gnu/"
-cp -v "./scripts/app_run.sh" "$APPDIR/AppRun"
-glib-compile-schemas "$APPDIR/$gsettings_schema_dir"
-./linuxdeployqt.AppImage "$APPDIR/usr/share/applications/cpu-x.desktop" -appimage -bundle-non-qt-libs -verbose=2
-#rm -rfv $APPDIR/{etc,var}
-find $APPDIR/
-find . -name '*.AppImage' -exec realpath {} \;
+gsettings_schema_dir="/usr/share/glib-2.0/schemas"
+#mkdir -pv "$APPDIR/$gtk_libdir" "$APPDIR/$gdk_pixbuf_dir" "$APPDIR/$terminfo_dir" "$APPDIR/$gsettings_schema_dir"
+#cp -Rv "$gtk_libdir/gtk-3.0" "$APPDIR/$gtk_libdir/"
+#cp -Rv "$gdk_pixbuf_moduledir" "$gdk_pixbuf_cache_file" "$APPDIR/$gdk_pixbuf_dir/"
+#cp -Rv "/usr/share/"{glib-2.0,terminfo,themes} "$APPDIR/usr/share/"
+#cp -v "/usr/lib/x86_64-linux-gnu/librsvg"* "$APPDIR/usr/lib/x86_64-linux-gnu/"
+safeCopy "$gtk_libdir/gtk-3.0" \
+	"$gdk_pixbuf_moduledir" \
+	"$gdk_pixbuf_cache_file" \
+	"/usr/share/"{glib-2.0,terminfo,themes} \
+	"/usr/lib/x86_64-linux-gnu/librsvg"* \
+	"$APPDIR"
+runCmd cp -v "./scripts/app_run.sh" "$APPDIR/AppRun"
+runCmd glib-compile-schemas "$APPDIR/$gsettings_schema_dir"
 
-# wget -c https://github.com/probonopd/uploadtool/raw/master/upload.sh
-# chmod +x upload.sh
-# ./upload.sh CPU-X*.AppImage*
+runCmd wget -c -nv -O "linuxdeployqt.AppImage" "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
+runCmd chmod a+x "linuxdeployqt.AppImage"
+unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
+#export VERSION=$(git describe --long --tags | sed 's/^v//;s/\([^-]*-g\)/r\1/;s/-/./g') # linuxdeployqt uses this for naming the file
+export VERSION="continuous"
+runCmd ./linuxdeployqt.AppImage "$APPDIR/usr/share/applications/cpu-x.desktop" -appimage -extra-plugins=iconengines,platformthemes/libqgtk3.so -verbose=2
